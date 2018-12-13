a016b626a0ae40f494eb17018dbb09e38ec28b7e
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2018-12-13 15:38:10 +0000

    Updating README with install and running instructions. Adding Python split script.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # dri-open-refine-split
-Scripts for splitting an exported XML file from Open Refine to create multiple xml files for batch ingest into DRI
+Scripts for splitting an exported XML file from Open Refine to create multiple xml files for batch ingest into DRI. So far just a Python script, but other options might be added in future.
+
+This tool should be used in conjuction with the Open Refine Dublin Core export template here https://gist.github.com/hellbunnie/dfca37537a80ec698a4cf9c773e4566a
+
+If you follow the instructions on this gist you will end up with a single large file with all of your xml records in it. it is necessary to split these into multiple individual files, one per DC record before ingesting into DRI (or another repository).
+
+Users on Windows machines are unlikely to have access to tool such as awk that make splitting the files relatively easy, so this script is provided to perform the splitting.
+
+If you have included a FILENAME line in your export template then this script will automatically use those filenames to create your xml files. Otherwise the files will be named 1.xml, 2.xml, etc.
+
+# Installation
+Install Python 3 for your system.
+
+To use the Graphical User Interface install the Python 3 Tkinter module.
+
+Clone the repository from Github.
+
+# Running
+You can run this tool by double-clicking on the script.py file. Alternatively, you can run it from the command line.
+
+Optional command-line parameters are --filename and --outputdir
+
+If you do not pass the intput filename and outputdir on the command line you will be prompted to enter or select them. If you have Tkinter installed you can select the input file and output directory via a Graphical User Interface, otherwise they can be typed in on the command line.
+
+
---
+++ split.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+
+import argparse
+from os import path
+import re
+
+gui = True
+try:
+    from tkinter import filedialog
+    from tkinter import *
+except ImportError:
+    gui = False
+
+def main():
+    filename, outputdir = setup_params()
+    process_file(filename, outputdir)
+
+
+## Get the filename and outputdir by hook or by crook!
+## If the machine has Tkinter installed we can use a graphical
+## file browser. Otherwise the user can enter the params on the
+## command line or via an input prompt
+## The multiple methods are there to make it as easy as possible
+## for users with different needs and experience to use the tool
+def setup_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', help='The file to be split')
+    parser.add_argument('--outputdir', help='Directory in which to create the split xml files')
+    args = parser.parse_args()
+
+    if gui:
+        filename = args.filename or filedialog.askopenfilename(initialdir = ".",
+                                    title = "Select file",
+                                    filetypes = (("xml files","*.xml"),("all files","*.*")))
+        outputdir = args.outputdir or filedialog.askdirectory(initialdir = ".",
+                                    title = "Select output diretory")
+    else:
+        filename = args.filename or input("Please enter the input filename ")
+        outputdir = args.outputdir or input("Please enter the output directory ")
+
+    filename = filename.rstrip()
+    outputdir = outputdir.rstrip()
+
+    if path.isfile(filename) and path.isdir(outputdir):
+        print("Processing input file ", filename)
+        print("Creating output xml files in ",outputdir)
+        return filename,outputdir
+    else:
+        print("Invalid input or output location")
+        exit(1)
+
+
+## processes input file and creates output files
+## Reads the file line by line look for start indicator
+## This should be either a line beginning FILENAME followed by the filename
+## or if that is not found we will fall back to searching for the xml header
+## line and will name files sequentially
+def process_file(inputfile, outputdir):
+    filecount = 1
+    outfile = False
+    with open(inputfile) as i:
+        for line in i:
+
+            # ignore blank lines
+            searchObj = re.search( r'^\s*$', line)
+            if searchObj:
+                continue
+
+            # Set the outfile name
+            searchObj = re.search( r'^FILENAME (.*)', line)
+            if searchObj:
+                outfile = path.join(outputdir, searchObj.group(1).rstrip() + ".xml")
+                continue
+
+            searchObj = re.search( r'^<qualifieddc', line)            
+            if searchObj and not outfile:
+                outfile = path.join(outputdir, str(filecount) + ".xml")
+
+            # Write the line to the current outfile
+            o = open(outfile, "a")
+            o.write(line)
+            o.close
+
+            # If we have reached the end of the file reset the filename
+            searchObj = re.search( r'^</qualifieddc', line)
+            if searchObj:
+                outfile = False
+                filecount = filecount+1
+
+
+
+if __name__ == '__main__':
+    main()
+
